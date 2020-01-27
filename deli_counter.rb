ea53b1249036dbaaf2b1d3c8c5aa511a11490547
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 16:00:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,12 +11,23 @@ def line(katz_deli)
   end
 end
 
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
 
 
 
-def take_a_number(deli, name)
-  deli << name
-  puts "Welcome, #{name}. You are number #{deli.length} in line."
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_delideli.length} in line."
 end
 
 def now_serving(deli)
