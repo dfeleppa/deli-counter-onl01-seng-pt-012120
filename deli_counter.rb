6a5a8480c250a5e1a58c4d9cad64e259e0c81338
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:58:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,19 +10,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.index} in line."
-end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  elseif katz_deli.length >= 1 
-    puts "Currently serving #{katz_deli[0]}"
-    katz_deli.shift
-  end
-end 
 
 def line(deli)
   if deli.empty?
