0e67feb44cae453e6719261778ae4cc9367fc8bc
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:48:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,17 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    katz_deli.join
+    katz_deli.join("")
 end
 
-def take_a_number(line, name)
+def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{line.length+1} in line."
   line >> name
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+    
+  puts "Currently serving"
+end  
