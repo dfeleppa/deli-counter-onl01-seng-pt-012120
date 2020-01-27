ef1225c580108e6c9f04f1f6c0ab46f1de5fc01a
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 16:03:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,7 @@
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
-  else
-  #elsif katz_deli.length >= 1
+  elsif katz_deli.length >= 1
       current_line = "The line is currently:"
     katz_deli.each.with_index(1) { |person, i|
       current_line << " #{i}. #{person}"
