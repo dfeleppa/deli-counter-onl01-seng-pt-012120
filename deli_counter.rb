a175f163f7d2ffad2faa3c65b14f8315bad96c69
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 16:06:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,14 +10,9 @@ def line(katz_deli)
   end
 end
 
-
-
-
-
 def take_a_number(katz_deli, name)
   katz_deli << name
- # puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  puts "Welcome, #{name}. You are number #{name.index+1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
