baea70df59a0d870ffab71fc6de57af644433958
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 16:05:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index} in line."
 end
 
-
-
-
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
