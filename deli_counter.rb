093838343b1006753c4e6e5db3ac6435c667fd83
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:50:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
-    
-  puts "Currently serving"
+  elseif katz_deli.length >= 1 
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
 end  
