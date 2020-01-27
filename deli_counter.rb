5e049a9e25cc19761a0da1febf81de179aad1f62
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:52:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    katz_deli.join("katz_deli")
+    katz_deli.join(" #{name.index + 1}. ")
+    
 end
 
 def take_a_number(katz_deli, name)
