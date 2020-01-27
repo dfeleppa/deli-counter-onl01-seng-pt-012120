c323adde10454b2daa4808121096104cbc1bc3ed
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:45:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
-def line 
-  if line.length = 0
-    puts 
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif line.length >= 1
+    line
 end
 
 def take_a_number(line, name)
-  puts #{name}
-  puts "#{line.length+1}"
+  puts "Welcome, #{name}. You are number #{line.length+1} in line."
   line >> name
 end
